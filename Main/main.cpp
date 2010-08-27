2d7e16225e755885a921a080ae5af896faef3ddd
Ben <eatingstaples@gmail.com>
2010-08-27 11:20:49 -0400

    Black window displays for 2 seconds - success!

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,5 +1,6 @@
 #include <boost/thread.hpp>
 #include <boost/date_time.hpp>
+#include <SDL/SDL.h>
 
 using namespace boost;
 
@@ -8,13 +9,16 @@ static void MakeWindow();
 int main()
 {
 	MakeWindow();
-	for(;;)
-		this_thread::sleep(posix_time::millisec(50));
+	SDL_Delay(2000);
 
+	SDL_Quit();
 	return 0;
 }
 
 void MakeWindow()
 {
-
+	SDL_Surface* screen;
+	SDL_Init(SDL_INIT_EVERYTHING);
+	screen = SDL_SetVideoMode(1024, 768, 32, SDL_SWSURFACE);
+	SDL_Flip(screen);
 }
