94aafa0d44f5b59b7b4299a5d1ee6465bd79be93
Ben <eatingstaples@gmail.com>
2010-08-28 17:46:23 -0400

    Added some obvious Snake members

--- Main/Snake.hpp
+++ Main/Snake.hpp
@@ -1,10 +1,22 @@
 #ifndef __SNAKE__
 #define __SNAKE__
 
+#include <vector>
 #include <SDL/SDL.h>
 
 class Snake
 {
+private:
+	enum Direction
+	{
+		left,
+		right,
+		up,
+		down
+	};
+	unsigned int length;
+	std::vector<Direction> path;
+
 public:
 	void GetInput();
 	void Update();
