f1a03c2e34f503af43ca5733d54abc991418caf7
Ben <eatingstaples@gmail.com>
2010-09-04 23:02:14 -0400

    Added wall constructors

--- Main/Wall.cpp
+++ Main/Wall.cpp
@@ -1,4 +1,15 @@
+#include "Point.hpp"
 #include "Wall.hpp"
+
+Wall::Wall()
+{
+}
+Wall::Wall(Point loc, size_t w, size_t h)
+{
+	location = loc;
+	width = w;
+	height = h;
+}
 
 WorldObject::ObjectType Wall::GetObjectType() const
 {
--- Main/Wall.hpp
+++ Main/Wall.hpp
@@ -1,11 +1,15 @@
 #ifndef __Wall__
 #define __Wall__
 
+#include "Point.hpp"
 #include "WorldObject.hpp"
 
 class Wall : public WorldObject
 {
 public:
+	Wall();
+	Wall(Point location, size_t width, size_t height);
+
 	ObjectType GetObjectType() const;
 };
 
