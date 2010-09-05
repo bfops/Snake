20e64989bc37e55e11ae48db5c0ed17ee13a93f4
Ben <eatingstaples@gmail.com>
2010-09-05 18:33:44 -0400

    Various minor fixes

--- Main/Physics.cpp
+++ Main/Physics.cpp
@@ -9,36 +9,54 @@ namespace PhysicsWorld
 {
 	namespace
 	{
-		typedef list<WorldObject*> PhysicsObjectList;
-		list<WorldObject*> objects;
+		typedef vector<WorldObject*> PhysicsObjectList;
+		PhysicsObjectList objects;
 	}
 	static bool IsCollide(const WorldObject&, const WorldObject&);
 	static bool IsWithinBounds(const WorldObject&, const WorldObject&, int Point::* element, size_t WorldObject::* dimension);
 	static bool IsLeftWithinBounds(const WorldObject&, const WorldObject&);
 	static bool IsTopWithinBounds(const WorldObject&, const WorldObject&);
 
-	void AddObject(WorldObject& obj)
+	static bool object_exists(const WorldObject& obj)
 	{
-		#ifdef NDEBUG
 		for(PhysicsObjectList::iterator i = objects.begin(); i != objects.end(); ++i)
 		{
-			assert(*i != &obj);
+			if(*i == &obj)
+				return true;
 		}
-		#endif
+		return false;
+	}
+	void AddObject(WorldObject& obj)
+	{
+		assert(!object_exists(obj));
 		objects.push_back(&obj);
 	}
 	void RemoveObject(WorldObject& obj)
 	{
-		objects.remove(&obj);
+		for(PhysicsObjectList::iterator i = objects.begin(), end = objects.end(); i != end; ++i)
+		{
+			if(*i == &obj)
+			{
+				// sexy removal hack.
+				// it's linear-time either way,
+				// but this is faster in practice
+				swap(*i, *objects.rbegin());
+				objects.pop_back();
+
+				return;
+			}
+		}
+
+		// TODO: throw errors at this point
 	}
 	void Update()
 	{
 		// TODO: only check those that have moved
-		for(PhysicsObjectList::iterator collider = objects.begin(); collider != objects.end(); ++collider)
+		for(PhysicsObjectList::iterator collider = objects.begin(), end = objects.end(); collider != end; ++collider)
 		{
 			PhysicsObjectList::iterator collidee = collider;
 			++collidee;
-			for(PhysicsObjectList::iterator end = objects.end(); collidee != end; ++collidee)
+			for(; collidee != end; ++collidee)
 			{
 				if(IsCollide(**collider, **collidee))
 				{
