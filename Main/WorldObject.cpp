1891200563dbafe55173404e957ed5b12c06b463
Ben <eatingstaples@gmail.com>
2010-09-04 22:27:30 -0400

    Removed WorldObject.cpp

--- Main/WorldObject.cpp
+++
@@ -1,9 +0,0 @@
-#include <cassert>
-
-#include "WorldObject.hpp"
-
-WorldObject::ObjectType WorldObject::GetObjectType() const
-{
-	assert(!"No override specified! Panicpanicpanic");
-	return generic;
-}
