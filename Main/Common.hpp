142be18e3a7ec80ea5cd4eff436e7f06605f23a5
Clark Gaebel <cg.wowus.cg@gmail.com>
2010-09-12 12:31:23 -0400

    Yay static.

--- Main/Common.hpp
+++ Main/Common.hpp
@@ -3,4 +3,4 @@
 #define countof(array) (sizeof(array) / sizeof(array[0]))
 
 #define DEF_CONSTANT(type, name, value) \
-	inline type name() { return value; }
+	static inline type name() { return value; }
