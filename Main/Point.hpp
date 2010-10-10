4b28ea1d02b6993e8008472387fe388913b8a0c1
Ben <eatingstaples@gmail.com>
2010-10-10 08:29:56 -0400

    Removed Main from gingerbread-in-c fork

--- Main/Point.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "Vector2D.hpp"
-
-typedef Vector2D Point;
--- Main/Side.cpp
+++
@@ -1,7 +0,0 @@
-#include "Side.hpp"
-
-void Side::ApplyVector(Vector2D vector, unsigned int scale)
-{
-	min.x += vector.x * scale;
-	min.y += vector.y * scale;
-}
