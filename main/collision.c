563d925eb64661d291466636ea1b8a41488a5d06
Ben <eatingstaples@gmail.com>
2010-10-15 20:39:39 -0400

    Just making collision detection more legible

--- main/collision.c
+++ main/collision.c
@@ -2,7 +2,7 @@
 
 bool does_collide(const CollidableObject* restrict obj1, const CollidableObject* restrict obj2)
 {
-	#define TESTBOUNDS(m) ((obj1->min.m <= obj2->min.m) ? (obj2->min.m < obj1->max.m) : (obj1->min.m < obj2->max.m))
+	#define TESTBOUNDS(m) (obj1->min.m < obj2->max.m && obj2->min.m < obj1->max.m)
 	return (TESTBOUNDS(x) && TESTBOUNDS(y));
 	#undef TESTBOUNDS
 }
