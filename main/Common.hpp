8fd682168a0e75920ce7c4d749bb38195d38b3ea
Ben Foppa <eatingstaples@gmail.com>
2010-12-16 07:16:16 -0500

    Switched from using built-in round() function (Linux only) to a custom-defined intRound() function

--- main/Common.hpp
+++ main/Common.hpp
@@ -2,6 +2,12 @@
 
 #define countof(array) (sizeof(array) / sizeof(array[0]))
 
+// round to nearest integer
+static inline int intRound(const double d)
+{
+	return (d + 0.5);
+}
+
 // execute _thingsToDo_ while _mutex_ is locked
 #define DOLOCKED(mutex, thingsToDo) \
 	mutex.lock(); \
