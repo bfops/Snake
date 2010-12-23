cbd7ab40ba758cd8ca6425d8b331b20bf06482e9
Ben Foppa <eatingstaples@gmail.com>
2010-12-23 14:24:08 -0500

    Bugfix: Calling Snake::Turn between Snake::Init call and Snake::AddSegment call could cause segfaulting, since there is no head

--- main/Snake.cpp
+++ main/Snake.cpp
@@ -89,15 +89,15 @@ void Snake::Init(ZippedUniqueObjectList& gameObjects)
 }
 
 void Snake::Reset(ZippedUniqueObjectList& gameObjects)
-{	
+{
 	DOLOCKED(pathMutex,
 		DOLOCKEDZ(gameObjects,
 			gameObjects.RemoveRange(path.begin(), path.end());
 		)
 		path.clear();
-	)
 
-	Init(gameObjects);
+		Init(gameObjects);
+	)
 }
 
 void Snake::RemoveTail(ZippedUniqueObjectList& gameObjects)
